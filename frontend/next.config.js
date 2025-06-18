92b092abdcb01fb2aefa214b542e51dfbc72d955
Letimba <142944577+Letimba@users.noreply.github.com>
2025-06-18 21:08:40 +0200

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { appDir: true },
   images: { domains: ['instagram.com'] }
 };
+
 module.exports = nextConfig;
+
